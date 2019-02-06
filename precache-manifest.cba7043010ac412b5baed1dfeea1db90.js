ce801ba0449dd06987db097ccc64245840a3dea5
DipanshKhandelwal <deepanshrocks438@gmail.com>
2019-02-06 10:53:25 +0530

    Updates

---
+++ precache-manifest.cba7043010ac412b5baed1dfeea1db90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9dfb967d9e8474cf391",
+    "url": "/sortable-list/static/js/runtime~main.c9dfb967.js"
+  },
+  {
+    "revision": "aaead5ed8fbdf02e504a",
+    "url": "/sortable-list/static/js/main.aaead5ed.chunk.js"
+  },
+  {
+    "revision": "1e1fe48fec5c47babb20",
+    "url": "/sortable-list/static/js/1.1e1fe48f.chunk.js"
+  },
+  {
+    "revision": "aaead5ed8fbdf02e504a",
+    "url": "/sortable-list/static/css/main.3492df0e.chunk.css"
+  },
+  {
+    "revision": "de0a5fff1ff97327083f59c44d5aa079",
+    "url": "/sortable-list/index.html"
+  }
+];
